3ae07f867943fae017c9919cc9fed67165b07587
Martin Černý <cernym68@fel.cvut.cz>
2025-01-06 16:21:21 +0100

    🔧 rc4

--- frontend/time-capsule/src/config/api.config.ts
+++ frontend/time-capsule/src/config/api.config.ts
@@ -5,7 +5,7 @@ const API_CONFIG = {
         : 'http://actual-production-url.com/api',  // Produkční URL
 
      */
-    BASE_URL: 'https://cors-anywhere.herokuapp.com/http://130.61.191.237:8080/',
+    BASE_URL: 'http://130.61.191.237:8080',
     // BASE_URL: 'https://localhost:8080',
 
     ENDPOINTS: {
